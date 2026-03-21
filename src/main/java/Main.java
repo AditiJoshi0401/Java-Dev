7c2ca1377e4708825a64f1e173f7c2b0df0f9584
Aditi Joshi <146647473+AditiJoshi0401@users.noreply.github.com>
2026-03-21 06:13:24 +0000

    main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,44 +1,5 @@
 public class Main {
-    public static void main(String args[]){
-        int a =10;
-        int b =20;
-        String statemen1 = "A is better than B";
-        String statemen2 = "B is better than A";
-
-        /*
-            if (<condition to check> && <cond2> || <cond3>) {
-                <do this part if true>
-            }else{
-                <do this part if false>
-            }
-        */
-
-        // 10 > 20 -> not true
-        if (a>b){
-            System.out.println(statemen1);
-        }else{
-            System.out.println(statemen2);
-        }
-
-        // ternary operator
-        // (condition) ? <if true exec this statement> : <if false exec this statement>
-
-        System.out.println((a>b)? "A is Greater!" : "B is Greater!" );
-
-        // switch case
-
-        String condition_value = "Gibrish";
-
-        switch (condition_value) {
-            case "Add":
-                System.out.println("Addition of a and b. A+B=" + String.valueOf(a+b));
-                break;
-            case "Sub":                
-                System.out.println("Addition of b from a. A-B=" + String.valueOf(a-b));
-                break;
-            default:
-                System.out.println("Do no operation A="+String.valueOf(a)+" B="+String.valueOf(b));
-                break;
-        }
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
     }
 }
