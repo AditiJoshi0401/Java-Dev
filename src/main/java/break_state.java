c83afb22fb27086ab6107b789735245bf18d7b31
Aditi Joshi <146647473+AditiJoshi0401@users.noreply.github.com>
2026-03-21 06:15:26 +0000

    break_statement

---
+++ src/main/java/break_state.java
@@ -0,0 +1,54 @@
+public class decision_making {
+    public static void main(String args[]) {
+        int a = 10;
+        int b = 20;
+        String statemen1 = "A is better than B";
+        String statemen2 = "B is better than A";
+
+        /*
+         * if (<condition to check> && <cond2> || <cond3>) {
+         * <do this part if true>
+         * }else{
+         * <do this part if false>
+         * }
+         */
+
+        // 10 > 20 -> not true
+        if (a > b) {
+            System.out.println(statemen1);
+        } else {
+            System.out.println(statemen2);
+        }
+
+        // ternary operator
+        // (condition) ? <if true exec this statement> : <if false exec this statement>
+
+        System.out.println((a > b) ? "A is Greater!" : "B is Greater!");
+
+        // switch case
+        /*
+         * switch (condition_value){
+         * case condition_value_1:
+         * <do stuff>
+         * case condition_value_2:
+         * <do_stuff>
+         * default:
+         * <do stuff any ways if not match>
+         * }
+         */
+
+        String condition_value = "random";
+
+        switch (condition_value) {
+            default:
+                System.out.println("Do no operation A=" + String.valueOf(a) + " B=" + String.valueOf(b));
+                break;
+            case "Add":
+                System.out.println("Addition of a and b. A+B=" + String.valueOf(a + b));
+                break;
+            case "Sub":
+                System.out.println("Substraction of b from a. A-B=" + String.valueOf(a - b));
+                break;
+        }
+    }
+}
