07f3fc42c928a9f62b060be7db1f2764340f6cd3
Aditi Joshi <146647473+AditiJoshi0401@users.noreply.github.com>
2026-03-21 05:12:44 +0000

    more variables

--- src/main/java/variables.java
+++ src/main/java/variables.java
@@ -1,6 +1,16 @@
-public class variables{
-  public static void main(String[] args){
+public class variables {
+  public static void main(String[] args) {
     String name = "Aditi";
     System.out.println(name);
+
+    int A = -10;
+    double B = 3.14;
+    boolean C = true;
+    String pi = "PI";
+
+    System.out.println(A);
+    System.out.println(B);
+    System.out.println(C);
+    System.out.println(pi);
   }
 }
